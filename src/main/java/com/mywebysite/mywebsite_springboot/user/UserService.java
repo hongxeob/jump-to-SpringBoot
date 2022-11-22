1e23c6d8c8db371a34562eec6083694e4361792f
hongxeob <sjun2918@naver.com>
2022-11-22 21:46:42 +0900

    Feat : 회원가입

---
+++ src/main/java/com/mywebysite/mywebsite_springboot/user/UserService.java
@@ -0,0 +1,25 @@
+package com.mywebysite.mywebsite_springboot.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String password, String email) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+}
