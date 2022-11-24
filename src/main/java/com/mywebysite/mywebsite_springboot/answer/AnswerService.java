c10f611798fcb8f15992935974b8711f5aa2c59a
hongxeob <sjun2918@naver.com>
2022-11-24 16:13:56 +0900

    Feat : 수정과 삭제

--- src/main/java/com/mywebysite/mywebsite_springboot/answer/AnswerService.java
+++ src/main/java/com/mywebysite/mywebsite_springboot/answer/AnswerService.java
@@ -1,5 +1,6 @@
 package com.mywebysite.mywebsite_springboot.answer;
 
+import com.mywebysite.mywebsite_springboot.DataNotFoundException;
 import com.mywebysite.mywebsite_springboot.quesqtion.Question;
 import com.mywebysite.mywebsite_springboot.user.SiteUser;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.Optional;
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public Answer create(Question question, String content,SiteUser author) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
@@ -21,6 +22,25 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 
 }
