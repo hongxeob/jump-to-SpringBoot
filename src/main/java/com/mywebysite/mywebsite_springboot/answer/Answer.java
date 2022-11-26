83ce120eb82663bb9ac35cd4146c245eaebc0d54
hongxeob <sjun2918@naver.com>
2022-11-26 20:36:01 +0900

    Feat : 추천 기능 구현

--- src/main/java/com/mywebysite/mywebsite_springboot/answer/Answer.java
+++ src/main/java/com/mywebysite/mywebsite_springboot/answer/Answer.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -28,5 +29,8 @@ public class Answer {
     private SiteUser author;
 
     private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 
 }
